d48ffbc5b612078a55b1042632330d5eabf4673a
rtraborn <rtraborn@indiana.edu>
2016-09-12 18:14:06 -0400

    Need to make tweak to RAMPAGE_align_STAR.sh but all other scripts work.

--- scripts/RAMPAGE_align_STAR.sh
+++ scripts/RAMPAGE_align_STAR.sh
@@ -1,15 +1,13 @@
 #!/bin/bash
 
 ### Please enter information here (test files are entered by default) ###
-GENOME=../test/sample_genome/Osj.fa
-GENDIR=../test/sample_genome/ 
-FQ_DIR=../test/sample_fastq/demultiplexed
+GENOME=OsjChr1.fa
+GENDIR=`readlink -f ../test/sample_genome/`
+FQ_DIR=/scratch/rtraborn/RAMPAGE_pipeline//test/sample_fastq/demultiplexed_matched/clipped/tagDust_files
 OutFilterScore=0.60
 OutSamType=BAM
 NThreads=8
 ############
-
-cd $FQ_DIR
 
 echo "Indexing the genome"
 
@@ -17,11 +15,21 @@ STAR --runMode genomeGenerate --runThreadN $NThreads --genomeDir $GENDIR --genom
 
 echo "Aligning the RAMPAGE reads to the assembly"
 
-for i in `ls *R1.fastq | cut -d "." -f 1` ; do
+cd $FQ_DIR
 
-echo $i 
+for i in `ls *.fastq_READ1.fq` ; do
+
+    j=`expr $i | cut -d "." -f 1`
+    k=`expr $i | cut -d "." -f 3,4`
+    l=${j}.R2.${k}.fastq_READ2.fq
+
+    echo $j
+    echo $k
+    echo $l
+
+echo "Aligning" $i $l
     
-STAR --runMode alignReads --runThreadN 8 --genomeDir $GENDIR --outFilterType Normal --readFilesIn ${i}.R1.fastq ${i}.R2.fastq --readMatesLengthsIn NotEqual --alignMatesGapMax 1000 --sjdbOverhang 39 --outFilterMultimapNmax 20 --outFilterMismatchNmax 2 --outFilterScoreMinOverLread $OutFilterScore --outSAMtype $OutSamType SortedByCoordinate --outSAMorder Paired --outFileNamePrefix $FQ_DIR/${i}.
+STAR --runMode alignReads --runThreadN 8 --genomeDir $GENDIR --outFilterType Normal --readFilesIn ${i} ${l} --readMatesLengthsIn NotEqual --alignMatesGapMax 1000 --sjdbOverhang 39 --outFilterMultimapNmax 20 --outFilterMismatchNmax 2 --outFilterScoreMinOverLread $OutFilterScore --outSAMtype $OutSamType SortedByCoordinate --outSAMorder Paired
 
 done
 
