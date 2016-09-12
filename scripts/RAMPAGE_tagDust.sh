d7a0f655ac11566babb8616be413e24c4d2cc9da
rtraborn <rtraborn@indiana.edu>
2016-09-12 14:42:59 -0400

    RAMPAGE_tagDust.sh updated and tested.

--- scripts/RAMPAGE_tagDust.sh
+++ scripts/RAMPAGE_tagDust.sh
@@ -1,25 +1,36 @@
 #!/bin/bash
 
 ### Please enter information here (test files are entered by default) ###
-fastqDir=../test/sample_fastq
-rRNA=../test/RAMPAGE_rRNA_example.fasta
+fastqDir=../test/sample_fastq/demultiplexed_matched/clipped
+rRNADir=../test/
 NThreads=6
 mySuffix=tagdust
 ############
+
+rRNA=`readlink -f ${rRNADir}/RAMPAGE_rRNA_example.fasta`
 
 echo "Beginning tagdust run"
 
 cd $fastqDir
 
-for i in `ls *R1.*.fastq | cut -d "." -f 2`;
+for i in `ls *.R1.*.clipped.fastq`;
 
 do
-     echo ${i}.fastq
+    echo ${i}
+    j=`expr $i | cut -d "." -f 1`
+    k=`expr $i | cut -d "." -f 3,4`
+    l=${j}.R2.${k}.fastq
 
-     tagdust -ref $rRNA -dust 97 -t $NThreads -fe 3 -1 R:N ${i}.R1.fastq ${i}.R2.fastq -o ${i}.$mySuffix.fastq
+     tagdust -ref $rRNA -dust 97 -t $NThreads -fe 3 -1 R:N ${i} ${l} -o $(basename $i .clipped.fastq).$mySuffix.fastq
 
-echo "tagDust run is complete!"
-     
 done
 
-    
+echo "tagDust run is complete!"    
+
+echo "Moving files into the new tagDust_files/ directory."
+
+mkdir tagDust_files
+
+mv *.tagdust.fastq* tagDust_files
+
+echo "Job complete!"
