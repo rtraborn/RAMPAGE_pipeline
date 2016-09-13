da8f399a1dddbccc1237861599312d3c024bac13
rtraborn <rtraborn@indiana.edu>
2016-09-12 23:23:30 -0400

    RAMPAGE_align_STAR.sh now in working order.

--- scripts/RAMPAGE_align_STAR.sh
+++ scripts/RAMPAGE_align_STAR.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 ### Please enter information here (test files are entered by default) ###
-GENOME=OsjChr1.fa
-GENDIR=`readlink -f ../test/sample_genome/`
+GENOME=../test/sample_genome/OsjChr1.fa
+GENDIR=/scratch/rtraborn/RAMPAGE_pipeline/test/sample_genome #obviously we need a relative, not full, path
 FQ_DIR=/scratch/rtraborn/RAMPAGE_pipeline//test/sample_fastq/demultiplexed_matched/clipped/tagDust_files
 OutFilterScore=0.60
 OutSamType=BAM
@@ -19,21 +19,22 @@ cd $FQ_DIR
 
 for i in `ls *.fastq_READ1.fq` ; do
 
-    j=`expr $i | cut -d "." -f 1`
-    k=`expr $i | cut -d "." -f 3,4`
-    l=${j}.R2.${k}.fastq_READ2.fq
-
-    echo $j
-    echo $k
-    echo $l
+    j=`expr $i | cut -d "." -f 1-4`
+    k=`expr $i | cut -d "." -f 3`
+    l=${j}.fastq_READ2.fq
 
 echo "Aligning" $i $l
     
-STAR --runMode alignReads --runThreadN 8 --genomeDir $GENDIR --outFilterType Normal --readFilesIn ${i} ${l} --readMatesLengthsIn NotEqual --alignMatesGapMax 1000 --sjdbOverhang 39 --outFilterMultimapNmax 20 --outFilterMismatchNmax 2 --outFilterScoreMinOverLread $OutFilterScore --outSAMtype $OutSamType SortedByCoordinate --outSAMorder Paired
+STAR --runMode alignReads --runThreadN 8 --genomeDir $GENDIR --outFilterType Normal --readFilesIn ${i} ${l} --readMatesLengthsIn NotEqual --alignMatesGapMax 1000 --sjdbOverhang 39 --outFilterMultimapNmax 20 --outFilterMismatchNmax 2 --outFilterScoreMinOverLread $OutFilterScore --outSAMtype $OutSamType SortedByCoordinate --outSAMorder Paired --outFileNamePrefix ${k}.
 
 done
 
-echo "Alignment is now complete!"
+echo "Alignment complete!"
 
+echo "Moving all alignment-associated files into /test/alignment."
 
+mkdir test_alignment
 
+mv *.out *.bam test_alignment
+
+echo "Job complete!"
